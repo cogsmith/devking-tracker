520479c4f23daa4fed2ed546dcefec9a5c920fbb
DAC <dac@cogsmith.com>
2021-04-05 01:01:07 -0400

    .

--- action.js
+++ action.js
@@ -103,7 +103,7 @@ if (0) {
 
 const App = {};
 
-App.Args = { loglevel: 'trace', logfancy: true, deleteothers: true };
+App.Args = { loglevel: 'trace', logfancy: true, deleteothers: false };
 
 App.LogFancy = false; if (App.Args.logfancy) { App.LogFancy = { colorize: true, singleLine: true, translateTime: 'SYS:yyyy-mm-dd|HH:MM:ss', ignore: 'hostname,pid', messageFormat: function (log, key, label) { let msg = log.msg ? log.msg : ''; let logout = chalk.gray(App.Meta.NameTag); if (msg != '') { logout += ' ' + msg }; return logout; } }; }
 App.Log = pino({ level: App.Args.loglevel, hooks: { logMethod: function (args, method) { if (args.length === 2) { args.reverse() } method.apply(this, args) } }, prettyPrint: App.LogFancy });
